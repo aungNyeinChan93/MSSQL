3a743ba2a1a4851d0123bf3e49db528ea108b563
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-10 10:58:54 +0630

    Feat: add orders

---
+++ Tuto_01/Features/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Tuto_01.Features
+{
+    public class Order
+    {
+
+        private readonly string _databaseStr = "Data Source=.;Initial Catalog=MyDatabase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;";
+
+        private SqlConnection _connection;
+
+        public Order( )
+        {
+            _connection = new SqlConnection(_databaseStr);
+        }
+
+        public void GetAllOrders()
+        {
+            DataTable table = new DataTable("Orders");
+            _connection.Open();
+            string query = @"SELECT [order_id]
+                              ,[customer_id]
+                              ,[order_date]
+                              ,[sales]
+                          FROM [dbo].[orders]";
+            SqlCommand cmd = new SqlCommand(query, _connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(table);
+
+            foreach (DataRow row in table.Rows)
+            {
+                Console.WriteLine($"Sale amount is {row["sales"]} \r\n");
+            }
+            _connection.Close();
+        }
+    }
+}
--- Tuto_01/Program.cs
+++ Tuto_01/Program.cs
@@ -4,6 +4,10 @@
 using Tuto_01.Features;
 
 Customers customer = new Customers();
+Order order = new Order();
 
 //customer.Create("Jojo","Myanmar",120);
-customer.GetAll();
+//customer.GetAll();
+
+
+order.GetAllOrders();
